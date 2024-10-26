320e2870a4a2c09d580d559dba3b1992e940bec3
manhpd <manh.pham@zyllem.com>
2024-10-26 21:48:55 +0700

    Fix minor bug

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -6,11 +6,13 @@ import { QuizContext } from "../store/quiz-contex";
 
 export default function Question() {
     // array of 4 answers ref
-    const { userAnswers } = useContext(QuizContext);
+    const { userAnswers, answerStatus } = useContext(QuizContext);
     
     const activeQuestionIndex = userAnswers.length;
     const title = QUESTIONS[activeQuestionIndex].text;
     const key = userAnswers.length;
+
+   
     return (
         <div id="question">
                 <h2>Question {activeQuestionIndex + 1}</h2>
--- src/components/QuestionTimer.tsx
+++ src/components/QuestionTimer.tsx
@@ -6,9 +6,15 @@ export default function QuestionTimer({timeOut}: {  timeOut: number}) {
     const [timeLeft, setTimeLeft] = useState(timeOut);
     const { skipQuestion } = useContext(QuizContext);
   
+    const {answerStatus} = useContext(QuizContext);
     
     useEffect(() => {
         const timer  = setTimeout(skipQuestion, timeOut);
+        // if the user has selected an answer, change the timer to 2 seconds
+        if (answerStatus === "selected") {
+            clearTimeout(timer);
+            setTimeout(skipQuestion, 2000);
+        }
         return () => {
             clearTimeout(timer);
         };
@@ -18,6 +24,13 @@ export default function QuestionTimer({timeOut}: {  timeOut: number}) {
         const interval = setInterval(() => { 
             setTimeLeft((prev) => prev - 10);
         }, 10);
+
+        // if the user has selected an answer, change the timer left to 2 seconds
+        if (answerStatus === "selected") {
+            clearInterval(interval);
+            setTimeLeft(2000);
+            
+        }
 
         return () => {
             clearInterval(interval);
